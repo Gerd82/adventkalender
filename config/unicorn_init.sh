2ca5a5bb4483c6b3eaa516f075f3f249ddabfa3f
Gerhard Brandl <gb2@sms-siemag.at>
2014-11-30 23:37:31 +0100

    Unicorn Init Script korrigiert

--- config/unicorn_init.sh
+++ config/unicorn_init.sh
@@ -7,7 +7,7 @@ set -e
 TIMEOUT=${TIMEOUT-60}
 APP_ROOT=/var/www/adventkalender/current
 PID=$APP_ROOT/tmp/pids/unicorn.pid
-CMD="/usr/bin/unicorn -D -c $APP_ROOT/config/unicorn.rb"
+CMD="/usr/local/bin/unicorn -D -c $APP_ROOT/config/unicorn.rb"
 INIT_CONF=$APP_ROOT/config/init.conf
 action="$1"
 set -u
