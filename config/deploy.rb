6f39690b55dc4f70f5e4dcec51296ac31305e7b2
Gerhard Brandl <gb2@sms-siemag.at>
2014-12-01 16:18:29 +0100

    Änderungen im commit

--- config/deploy.rb
+++ config/deploy.rb
@@ -12,6 +12,16 @@ set :deploy_to, '/var/www/adventkalender'
 
 # Default value for :scm is :git
 set :scm, :git
+set :repo_url,    'https://github.com/Gerd82/adventkalender.git'
+
+set :deploy_user, 'deployer'
+
+# set :stage,       :production
+set :rails_env,   :production
+
+set :rvm_path, "$HOME/.rvm"
+set :rvm_ruby_string, "ruby-2.1.5@advent"
+set :rvm_type, :user  # Don't use system-wide RVM
 
 # Default value for :format is :pretty
 # set :format, :pretty
@@ -23,25 +33,77 @@ set :scm, :git
 # set :pty, true
 
 # Default value for :linked_files is []
-# set :linked_files, fetch(:linked_files, []).push('config/database.yml')
+set :linked_files, fetch(:linked_files, []).push('config/database.yml')
 
 # Default value for linked_dirs is []
-# set :linked_dirs, fetch(:linked_dirs, []).push('bin', 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
+set :linked_dirs, fetch(:linked_dirs, []).push('bin', 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
 
+# what specs should be run before deployment is allowed to
+# continue, see lib/capistrano/tasks/run_tests.cap
+set :tests, []
+
+# which config files should be copied by deploy:setup_config
+# see documentation in lib/capistrano/tasks/setup_config.cap
+# for details of operations
+set(:config_files, %w(
+  nginx.conf
+  database.example.yml
+  log_rotation
+  unicorn.rb
+  unicorn_init.sh
+))
+
+# which config files should be made executable after copying
+# by deploy:setup_config
+set(:executable_config_files, %w(
+  unicorn_init.sh
+))
+
+# files which need to be symlinked to other parts of the
+# filesystem. For example nginx virtualhosts, log rotation
+# init scripts etc.
+set(:symlinks, [
+  {
+    source: "nginx.conf",
+    link: "/etc/nginx/sites-enabled/#{fetch(:full_app_name)}"
+  },
+  {
+    source: "unicorn_init.sh",
+    link: "/etc/init.d/unicorn_#{fetch(:full_app_name)}"
+  },
+  {
+    source: "log_rotation",
+   link: "/etc/logrotate.d/#{fetch(:full_app_name)}"
+  }
+])
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
 
 # Default value for keep_releases is 5
-# set :keep_releases, 5
+set :keep_releases, 5
 
 namespace :deploy do
+  # make sure we're deploying what we think we're deploying
+  before :deploy, "deploy:check_revision"
+  # only allow a deploy with passing tests to deployed
+  before :deploy, "deploy:run_tests"
+  # compile assets locally then rsync
+  # after 'deploy:symlink:shared', 'deploy:compile_assets_locally'
+  after :finishing, 'deploy:cleanup'
 
-  desc "Symlink shared configs and folders on each release."
-  after   :update_code, :symlink_shared do
-    run "ln -nfs #{shared_path}/system/config/database.yml #{release_path}/config/database.yml"
-  end
+  # remove the default nginx configuration as it will tend
+  # to conflict with our configs.
+  before 'deploy:setup_config', 'nginx:remove_default_vhost'
 
-  after :restart, :clear_cache do
+  # reload nginx to it will pick up any modified vhosts from
+  # setup_config
+  after 'deploy:setup_config', 'nginx:reload'
+
+  # As of Capistrano 3.1, the `deploy:restart` task is not called
+  # automatically.
+  after 'deploy:publishing', 'deploy:restart'
+
+  after   :restart, :clear_cache do
     on roles(:web), in: :groups, limit: 3, wait: 10 do
       # Here we can do anything such as:
       # within release_path do
--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -15,7 +15,29 @@
 # server list. The second argument is a, or duck-types, Hash and is
 # used to set extended properties on the server.
 
-server 'brgerd.homenet.org', user: 'deployer',  port: 22020, roles: %w{web app db} #, my_property: :my_value
+set :stage, :production
+set :branch, "master"
+
+# app side by side. Also provides quick sanity checks when looking
+# at filepaths
+set :full_app_name, "#{fetch(:application)}_#{fetch(:stage)}"
+set :server_name, "www.brgerd.homenet.org brgerd.homenet.org"
+
+server 'brgerd.homenet.org', user: 'deployer',  port: 22020, roles: %w{web app db}, primary: true #, my_property: :my_value
+
+set :deploy_to, "/home/#{fetch(:deploy_user)}/apps/#{fetch(:full_app_name)}"
+
+# dont try and infer something as important as environment from
+# stage name.
+set :rails_env, :production
+
+# number of unicorn workers, this will be reflected in
+# the unicorn.rb and the monit configs
+set :unicorn_worker_count, 5
+
+# whether we're using ssl or not, used for building nginx
+# config file
+set :enable_ssl, false
 
 set :nginx_server_name, 'brgerd.homenet.org'
 
