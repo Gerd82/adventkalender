2462e3b035ee7143ac470a3af1706f922fa0aec5
Gerhard Brandl <gb2@sms-siemag.at>
2014-12-05 00:04:48 +0100

    Add Tag 5

--- app/controllers/adventkalender_controller.rb
+++ app/controllers/adventkalender_controller.rb
@@ -1,6 +1,6 @@
 class AdventkalenderController < ApplicationController
   def window
-    render partial: "window#{ params[:id] }"
+    render partial: "window#{ params[:id].to_i }"
     # render :js_show, locals: { partial: "window#{ params[:id] }"}
   end
 end
